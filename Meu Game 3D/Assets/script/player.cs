7374c16f686b434556207c14d83504b2a8fa11a7
maduasoares <eduarda.aleixo@escolar.ifrn.edu.br>
2024-09-02 16:06:20 -0300

    fase 1 modificada

--- Meu Game 3D/Assets/script/player.cs
+++ Meu Game 3D/Assets/script/player.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class player : MonoBehaviour
+using UnityEngine.SceneManagement;
+public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int velocidade = 10;
+    private Rigidbody rb;
+        
+    // Start is called before the frist frame update
     void Start()
     {
-        
+        Debug.Log("START");
+        TryGetComponent(out rb);
     }
 
-    // Update is called once per frame
+    // Update is called once per frame 
     void Update()
     {
+        Debug.Log("UPDATE");
+        float h = Input.GetAxis("Horizontal"); //-1 esquerda,0 nada, 1 direita
+        float v = Input.GetAxis("Vertical"); //-1 pra tras,0 nada, 1 pra frente
+        
+        UnityEngine.Vector3 direcao = new Vector3(h, 0, v);
+        rb.AddForce(direcao * velocidade * Time.deltaTime,ForceMode.Impulse);
+
+
+
+        if (transform.position.y <= -10)
+        {
+            //jogador caiu
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
         
     }
 }
